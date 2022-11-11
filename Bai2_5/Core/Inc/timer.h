3618f8088e4134e356d2aae0336f1a97d08a4b11
Govetachun <xuanbach1809@gmail.com>
2022-11-12 02:07:12 +0700

    second commit

---
+++ Bai2_5/Core/Inc/timer.h
@@ -0,0 +1,19 @@
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+#define TICK 10
+
+class Timer {
+private:
+	int counter;
+	bool signal;
+
+public:
+	Timer();
+
+	void setTimer(int duration);
+	void run();
+	bool hasSignal();
+};
+
+#endif /* INC_TIMER_H_ */
---
+++ Bai2_7/Core/Src/timer.cpp
@@ -0,0 +1,31 @@
+/*
+ * timer.cpp
+ *
+ *  Created on: Oct 13, 2022
+ *      Author: Admin
+ */
+#include "timer.h"
+
+Timer::Timer() {
+	this->counter = 0;
+	this->signal = false;
+}
+
+void Timer::run() {
+	if (this->counter > 0) {
+		this->counter -= 1;
+		if (this->counter == 0) {
+			this->signal = true;
+		}
+	}
+}
+
+bool Timer::hasSignal() {
+	return this->signal;
+}
+
+void Timer::setTimer(int duration) {
+	this->signal = false;
+	this->counter = duration / TICK;
+}
+
